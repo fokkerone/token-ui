3dccf500b2c9feebb98838f240f6f3d4dcdd1bfd
Volker Chartier <volker.chartier@rwe.com>
2023-06-01 11:32:28 +0200

    Story format 3 for Buttons

---
+++ libs/button/src/lib/button.stories.tsx
@@ -0,0 +1,30 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Root as Button } from './button';
+
+const meta: Meta<typeof Day> = {
+    /* 👇 The title prop is optional.
+     * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+     * to learn how to generate automatic titles
+     */
+    title: 'Button',
+    component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+    render: () => (
+        <div>
+            <Button size="default">Hello</Button>
+        </div>
+    ),
+};
